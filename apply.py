cc1f0c14d920befc951c336f6e26eb02533689a8
Scott Lagler <laglerscott@gmail.com>
2022-06-27 22:35:28 -0400

    feat: typelib import and apply

--- apply.py
+++ apply.py
@@ -2,24 +2,26 @@ import os
 from typing import Optional
 
 import binaryninja as bn
-from binaryninja import BinaryView, TypeLibrary
+from binaryninja import BinaryView, typelibrary
 
 
 def get_library_path() -> Optional[str]:
-    library_path = bn.TextLineField("Path to load type library:", "~/Desktop/libc.bntl")
+    library_path = bn.TextLineField("Path to load type library:", "~/Desktop/libcapstone.bntl")
     bn.get_form_input([library_path], "Apply Type Library Options")
-
-    if os.path.exists(os.path.expanduser(library_path.result)):
-        return library_path.result
+    res_path = os.path.expanduser(library_path.result)
+    if os.path.exists(res_path):
+        return res_path
     else:
         return None
 
 
-def apply_library(bv: BinaryView, typelib_handle: TypeLibrary):
+def apply_library(bv: BinaryView, typelib_handle: typelibrary.TypeLibrary):
     exported_objs = typelib_handle.named_objects
-    for qual_name, exported_type in exported_objs.items():
-        print(f"qual_name: {str(qual_name)} | type: {str(exported_type)}")
-    return
+    for qualified_name, exported_type in exported_objs.items():
+        for func in bv.functions:
+            if func.name == qualified_name.name[0]:
+                bn.log_debug(f"Found func: {qualified_name.name}")
+                func.apply_imported_types(func.symbol, exported_type)
 
 
 def load_library(bv: BinaryView):
@@ -27,7 +29,8 @@ def load_library(bv: BinaryView):
     if lib_path is None:
         bn.log_error(f"Supplied path was invalid")
         return
-    typelib_handle = TypeLibrary.load_from_file(lib_path)
+    bn.log_debug(f"lib_path: {lib_path}")
+    typelib_handle = typelibrary.TypeLibrary.load_from_file(lib_path)
     bv.add_type_library(typelib_handle)
     apply_library(bv, typelib_handle)
-    return
+    bv.update_analysis()
