f7596aa102faf8dbef64d3d5f04b6a0553695dff
Scott Lagler <laglerscott@gmail.com>
2022-10-15 17:57:34 -0400

    fix: types are now properly applied to functions

--- apply.py
+++ apply.py
@@ -2,7 +2,7 @@ import os
 from typing import Optional
 
 import binaryninja as bn
-from binaryninja import BinaryView, typelibrary
+from binaryninja import BinaryView, TypeLibrary
 
 
 def get_library_path() -> Optional[str]:
@@ -15,13 +15,13 @@ def get_library_path() -> Optional[str]:
         return None
 
 
-def apply_library(bv: BinaryView, typelib_handle: typelibrary.TypeLibrary):
+def apply_library(bv: BinaryView, typelib_handle: TypeLibrary):
     exported_objs = typelib_handle.named_objects
     for qualified_name, exported_type in exported_objs.items():
         for func in bv.functions:
-            if func.name == qualified_name.name[0]:
-                bn.log_debug(f"Found func: {qualified_name.name}")
-                func.apply_imported_types(func.symbol, exported_type)
+            if func.name == qualified_name:
+                bn.log_debug(f"Found func: {qualified_name}")
+                func.set_user_type(exported_type)
 
 
 def load_library(bv: BinaryView):
@@ -30,7 +30,7 @@ def load_library(bv: BinaryView):
         bn.log_error(f"Supplied path was invalid")
         return
     bn.log_debug(f"lib_path: {lib_path}")
-    typelib_handle = typelibrary.TypeLibrary.load_from_file(lib_path)
+    typelib_handle = TypeLibrary.load_from_file(lib_path)
     bv.add_type_library(typelib_handle)
     apply_library(bv, typelib_handle)
     bv.update_analysis()
